fbd1183be9e46636e500adf5f5bec015e3826c32
AnyaDali <91633048+AnyaDali@users.noreply.github.com>
2022-02-02 17:17:37 +0300

    Add files via upload

---
+++ fileInclude.h
@@ -0,0 +1,17 @@
+#ifndef FILE_INCLUDE
+#define FILE_INCLUDE
+
+#include <iostream>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include <memory.h>
+
+#include <list>
+
+#include <vector>
+
+#include <algorithm>
+
+#endif
